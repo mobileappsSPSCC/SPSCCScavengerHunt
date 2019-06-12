9c242a0900e2cf3d0d1a9724bbf375ad624c1d0d
Michael Haensel <mhaensel@spscc.edu>
2019-06-12 13:57:05 -0700

    Set enableHighAccuracy to False to debug maps on iOS

--- MapAppSPSCC/www/scripts/index.js
+++ MapAppSPSCC/www/scripts/index.js
@@ -84,7 +84,7 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay) {
 }*/
 
 function initMap() {
-    navigator.geolocation.getCurrentPosition(onGeolocationSuccess, onGeolocationFailure, { timeout: 5000 });
+    navigator.geolocation.getCurrentPosition(onGeolocationSuccess, onGeolocationFailure, { enableHighAccuracy: false, timeout: 5000 });
 
 };
 
